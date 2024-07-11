729be0777140a54e794e7460fef795fb745f32aa
Fidel76y <21082@student.embuni.ac.ke>
2024-07-11 16:00:41 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var ejs = require('ejs')
+
+var app = express();
+
+app.use(express.static('public'));
+app.set('View engine','ejs');
+
+app.listen(8080);
+
+app.get('/', function(req, res){
+
+    res.render('pages/Index');
+})
